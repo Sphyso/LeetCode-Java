0539dd37085c7049733ef33a48280bb0cf8f4388
Sifiso Hadebe <ST10131148@rcconnect.edu.za>
2024-10-30 18:15:00 +0200

    Initial Commit: Problem 300

---
+++ LeetCodePrac/src/LeetCode.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;  
+public class LeetCode {
+
+	public static void main(String[] args) {
+		
+		int nums[] = {10,9,2,5,3,7,101,18};
+		ArrayList<Integer> tempIndex = new ArrayList<Integer>();
+		int length = nums.length;
+		int tempRef[] = new int[length];
+		
+		for (int i = 0; i < nums.length; i++) {
+			tempRef[i] = -1;
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
