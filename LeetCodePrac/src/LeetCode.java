3fc9c683452cd7a666b99a6365a65eaa4e8d9abe
Sifiso Hadebe <ST10131148@rcconnect.edu.za>
2024-10-31 19:52:39 +0200

    Longest Increasing Subsequence: 300: ChatGPT answer, not tested

--- LeetCodePrac/src/LeetCode.java
+++ LeetCodePrac/src/LeetCode.java
@@ -3,20 +3,49 @@ public class LeetCode {
 
 	public static void main(String[] args) {
 		
-		int nums[] = {10,9,2,5,3,7,101,18};
-		ArrayList<Integer> tempIndex = new ArrayList<Integer>();
-		int length = nums.length;
-		int tempRef[] = new int[length];
+		int nums[] = {4,10,4,3,8,9};
+		LeetCode obj = new LeetCode();
+		System.out.print(obj.lengthOfLIS(nums)); 
 		
-		for (int i = 0; i < nums.length; i++) {
-			tempRef[i] = -1;
-		}
-		
-		
-		
-		
-		
-
 	}
+	
+	public static int lengthOfLIS(int[] nums) {
+        // `tempList` will store the smallest tail values of increasing subsequences
+        ArrayList<Integer> tempList = new ArrayList<>();
 
+        for (int num : nums) {
+            // Find the correct position of `num` in tempList using binary search
+            int pos = binarySearch(tempList, num);
+
+            // If `num` is greater than all elements in tempList, add it
+            if (pos == tempList.size()) {
+                tempList.add(num);
+            } else {
+                // Replace the element at `pos` with `num` to maintain the smallest possible tails
+                tempList.set(pos, num);
+            }
+        }
+
+        // Length of `tempList` will be the length of the longest increasing subsequence
+        return tempList.size();
+    }
+
+    // Binary search helper function
+    private static int binarySearch(ArrayList<Integer> list, int target) {
+        int left = 0, right = list.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (list.get(mid) == target) {
+                return mid;
+            } else if (list.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left; // This gives the position to replace or add `target`
+    }
 }
